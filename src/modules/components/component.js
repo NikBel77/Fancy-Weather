98d2ae35df62bcb5a9d84f17ba845dacbec9b860
Nikita Belyakov <nikita.belyakov.38@ya.ru>
2019-12-10 00:14:45 +0300

    feat: add control base control panel

--- src/modules/components/component.js
+++ src/modules/components/component.js
@@ -13,4 +13,17 @@ export default class Component {
         document.querySelector('.' + this.wrapperClassName).appendChild(wrapper);
 
     }
+
+    createElements(objElements, parentClassName) {
+
+        const parrent = document.querySelector('.' + parentClassName);
+        for (let key in objElements) {
+
+            const element = document.createElement(objElements[key].tag);
+            element.classList.add(objElements[key].class);
+            parrent.appendChild(element);
+
+        }
+
+    }
 }
--- src/modules/components/controls.js
+++ src/modules/components/controls.js
@@ -9,6 +9,31 @@ export default class Controls extends Component {
         this.targetClass = 'controls'
         this.createWrapper(this.targetClass);
 
+        this.elements = {
+
+            panel: { tag: 'div', class: 'controls__panel' },
+            search: { tag: 'div', class: 'controls__search' }
+
+        };
+        this.createElements(this.elements, this.targetClass);
+
+        this.panelElements = {
+
+            buttonImg: { tag: 'button', class: 'controls__btn-img' },
+            buttonLang: { tag: 'button', class: 'controls__btn-lang' },
+            buttonDeg: { tag: 'button', class: 'controls__btn-deg' }
+
+        };
+        this.createElements(this.panelElements, this.elements.panel.class);
+
+        this.searchElements = {
+
+            searchInput: { tag: 'input', class: 'controls__inp' },
+            searchBtn: { tag: 'button', class: 'controls__btn-search' }
+
+        }
+        this.createElements(this.searchElements, this.elements.search.class);
+
     }
 
 }
